a04027de75a1157d4401e24fbaac439f094f5deb
tyx1703 <tyx1703@gmail.com>
2020-07-26 13:25:47 +0800

    test: button unit test

--- test/unit/Button.test.js
+++ test/unit/Button.test.js
@@ -2,8 +2,76 @@ import { mount } from '@vue/test-utils';
 import Button from '@packages/Button';
 
 describe('Button', () => {
-  test('is a Vue instance', () => {
+  test('create', () => {
     const wrapper = mount(Button);
-    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.classes()).toContain('button');
+  });
+
+  test('plain button', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        plain: true,
+      },
+    });
+    expect(wrapper.classes()).toContain('button--plain');
+  });
+
+  test('primary button', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        primary: true,
+      },
+    });
+    expect(wrapper.classes()).toContain('button--primary');
+  });
+
+  test('compound button', () => {
+    const secondaryText = 'This is the secondary text.';
+    const wrapper = mount(Button, {
+      propsData: {
+        secondaryText,
+      },
+    });
+    expect(wrapper.find('.button__description').exists()).toBe(true);
+    expect(wrapper.find('.button__description').text()).toBe(secondaryText);
+  });
+
+  test('icon button', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'Mail',
+      },
+    });
+    expect(wrapper.find('.button__label>.icon.icon-Mail').exists()).toBe(true);
+  });
+
+  test('disabled button', () => {
+    const mockClickHandler = jest.fn();
+    const Context = {
+      template: `<Button @click="clickHandler" disabled />`,
+      components: { Button },
+      methods: {
+        clickHandler: mockClickHandler,
+      },
+    };
+    const wrapper = mount(Context);
+    const buttonWrapper = wrapper.findComponent(Button);
+    buttonWrapper.trigger('click');
+    expect(mockClickHandler.mock.calls.length).toBe(0);
+  });
+
+  test('click event', () => {
+    const mockClickHandler = jest.fn();
+    const Context = {
+      template: `<Button @click="clickHandler" />`,
+      components: { Button },
+      methods: {
+        clickHandler: mockClickHandler,
+      },
+    };
+    const wrapper = mount(Context);
+    const buttonWrapper = wrapper.findComponent(Button);
+    buttonWrapper.trigger('click');
+    expect(mockClickHandler.mock.calls.length).toBe(1);
   });
 });
